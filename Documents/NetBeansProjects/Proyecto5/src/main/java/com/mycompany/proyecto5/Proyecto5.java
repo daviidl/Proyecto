0402a8e08817a5100ea147f6e55f644ad93bf37d
daviidl <david.lema.amado@colexio-karbo.com>
2023-12-13 02:40:41 +0100

    Version 1

---
+++ Documents/NetBeansProjects/Proyecto5/src/main/java/com/mycompany/proyecto5/Proyecto5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto5;
+
+/**
+ *
+ * @author David
+ */
+public class Proyecto5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
